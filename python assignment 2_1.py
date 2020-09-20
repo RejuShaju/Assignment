4daf56b452c9b121bff13c37d0510023751b3d56
RejuShaju <70476238+RejuShaju@users.noreply.github.com>
2020-09-20 11:32:32 -0700

    Python assignment 2

---
+++ python assignment 2_1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+"""""
+question 
+"""""
+for i in range(5):
+    for j in range (i):
+        print ("*",end=" ")
+    print("")
+    
+for k in range(5,0,-1):
+    for l in range (k):
+        print ("*",end=" ")
+    print("")
+
+
+# In[27]:
+
+
+a = 'ineuron'
+rev_a = a[::-1]
+print(rev_a)
+
+
+# In[ ]:
+
+
+
+
