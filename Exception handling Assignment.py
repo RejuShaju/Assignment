a765e447d5d0637f51b5e467b37c31f22a2fec0c
RejuShaju <70476238+RejuShaju@users.noreply.github.com>
2020-09-22 09:24:53 -0700

    Exception handling Assignment

---
+++ Exception handling Assignment.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+subjects=["Americans ","Indians"]
+verbs=["play","watch"]
+objects=["Baseball","Cricket"]
+
+for x in subjects:
+    for y in verbs:
+        for z in objects:
+            print(x,y,z)
+
+
+# In[ ]:
+
+
+
+
